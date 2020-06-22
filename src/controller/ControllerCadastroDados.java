807db7bdac2350ec4bafeed986285bab2724823e
christian <christian@projetabrasil.net.br>
2020-06-22 12:13:44 -0300

    Remoção da tabela de carros, e cadastrando somente 1 carro por cliente

--- src/controller/ControllerCadastroDados.java
+++ src/controller/ControllerCadastroDados.java
@@ -1,267 +1,104 @@
 package controller;
 
+import java.util.ArrayList;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
 import model.banco.BaseDAO;
-import model.dao.veiculos.CarroDAO;
 import model.dao.veiculos.MarcaDAO;
 import model.dao.veiculos.ModeloDAO;
 import model.vo.cliente.ClienteVO;
 import model.vo.veiculo.CarroVO;
 import model.vo.veiculo.MarcaVO;
 import model.vo.veiculo.ModeloVO;
-import util.constantes.Colunas;
-import util.constantes.ConstHelpers;
 import util.constantes.ConstInicio;
-import util.helpers.Modificacoes;
 import view.panels.cadastro.subCadastro.PanelzinhoCadastroDados;
-
-import javax.swing.*;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
-import java.util.ArrayList;
 
 public class ControllerCadastroDados {
 
-    private final PanelzinhoCadastroDados cadastroView;
-    private final BaseDAO<CarroVO> daoCarro;
-    private ArrayList<CarroVO> linhas;
-    private ArrayList<CarroVO> lista;
-    private int id;
-    private int listaCheia;
+	private final PanelzinhoCadastroDados cadastroView;
 
-    public ControllerCadastroDados(JPanel panel) {
-        this.cadastroView = (PanelzinhoCadastroDados) panel;
-        linhas = new ArrayList<>();
-        lista = new ArrayList<>();
-        daoCarro = new CarroDAO();
-    }
+	public ControllerCadastroDados(JPanel panel) {
+		this.cadastroView = (PanelzinhoCadastroDados) panel;
+	}
 
-    public void atualizarTabela() {
-        limparTabela();
+	/**
+	 * Pega o formulario dos dados do Cliente na tela
+	 *
+	 * @return ClienteVO
+	 */
+	public ClienteVO getFormCliente() {
+		try {
+			String nome = cadastroView.getTxtNome().getText();
+			String cpf = cadastroView.getTxtCPF().getText();
+			String rg = cadastroView.getTxtRG().getText();
+			String email = cadastroView.getTxtEmail().getText();
+			String fone = cadastroView.getTxtTelefone().getText();
+			return new ClienteVO(nome, cpf, rg, email, fone);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-        if (ConstHelpers.FLAG == 1) {
-            lista = daoCarro.consultar(String.valueOf(id));
-        }
+	/**
+	 * Pega o formulario de cadastro de Carro
+	 * 
+	 * @return CarroVO
+	 */
+	public CarroVO getFormCarro(JTable table) {
+		String placa = cadastroView.getTxtPlaca().getText();
+		String cor = cadastroView.getCbCor().getSelectedItem().toString();
+		MarcaVO marca = (MarcaVO) cadastroView.getCbMarca().getSelectedItem();
+		ModeloVO modelo = (ModeloVO) cadastroView.getCbModelo().getSelectedItem();
+		modelo.setMarca(marca);
+		return new CarroVO(placa, cor, modelo);
+	}
 
-        DefaultTableModel model = (DefaultTableModel) cadastroView.getTable().getModel();
+//		ADICIONA COMBO BOX A TABELA
+//    private void addComboBoxTable_MarcaVO() {
+//        JTable table = cadastroView.getTable();
+//        TableColumn tableColumn = table.getColumnModel().getColumn(1);
+//        tableColumn.setCellEditor(new DefaultCellEditor(new JComboBox<>(preencherCbx_Marca())));
+//
+//        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
+//        renderer.setToolTipText("CLIQUE PARA O COMBO BOX APARECER");
+//        tableColumn.setCellRenderer(renderer);
+//    }
 
-        Object[] novaLinha = new Object[4];
-        for (CarroVO carro : lista) {
+	public DefaultComboBoxModel<String> preencherCbx_Cores() {
+		ArrayList<String> cores = new ArrayList<>();
+		cores.add(ConstInicio.VAZIO);
+		cores.add("AMARELO");
+		cores.add("AZUL");
+		cores.add("BRANCO");
+		cores.add("BEGE");
+		cores.add("CINZA");
+		cores.add("CROMADO");
+		cores.add("LARANJA");
+		cores.add("MARROM");
+		cores.add("PRATA");
+		cores.add("PRETO");
+		cores.add("ROSA");
+		cores.add("ROXO");
+		cores.add("VERMELHO");
+		cores.add("VINHO");
+		return new DefaultComboBoxModel(cores.toArray());
+	}
 
-            novaLinha[0] = carro.getPlaca();
-            novaLinha[1] = carro.getModelo().getMarca();
-            novaLinha[2] = carro.getModelo();
-            novaLinha[3] = carro.getCor();
+	public DefaultComboBoxModel<MarcaVO> preencherCbx_Marca() {
+		BaseDAO<MarcaVO> bDAO = new MarcaDAO();
+		ArrayList<MarcaVO> list = bDAO.consultarTodos();
+		return new DefaultComboBoxModel(list.toArray());
+	}
 
-            model.addRow(novaLinha);
-        }
-    }
-
-    private void limparTabela() {
-        cadastroView.getTable().setModel(new DefaultTableModel(new Object[][]{}, Colunas.COLUNAS_CLIENTE));
-    }
-
-    /**
-     * Adiciona uma nova linha a tabela
-     */
-    public void addrow() {
-        DefaultTableModel model = (DefaultTableModel) cadastroView.getTable().getModel();
-        Object[] data = new Object[1];
-        model.addRow(data);
-    }
-
-    /**
-     * Pega o formulario dos dados do Cliente na tela
-     *
-     * @return ClienteVO
-     */
-    public ClienteVO getFormCliente() {
-        try {
-            String nome = cadastroView.getTxtNome().getText();
-            String cpf = cadastroView.getTxtCPF().getText();
-            String rg = cadastroView.getTxtRG().getText();
-            String email = cadastroView.getTxtEmail().getText();
-            String fone = cadastroView.getTxtTelefone().getText();
-            return new ClienteVO(nome, cpf, rg, email, fone);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * Pega a lista de Carros da tabela
-     *
-     * @param table JTable
-     * @return CarroVO
-     */
-    public CarroVO getFormCarro(JTable table) { // TODO ALTERAR PARA PEGAR OS CAMPOS
-        int row = table.getRowCount();
-        DefaultTableModel model = (DefaultTableModel) cadastroView.getTable().getModel();
-        String primeiraPlaca = null;
-        String primeiraCor = null;
-        ModeloVO primeiroModelo = null;
-        for (int i = 0; i < row; i++) {
-            String placa = (String) model.getValueAt(i, 0);
-            MarcaVO marca = (MarcaVO) model.getValueAt(i, 1);
-            ModeloVO modelo = (ModeloVO) model.getValueAt(i, 2);
-            String cor = (String) model.getValueAt(i, 3);
-            if (marca != null) {
-                modelo.setMarca(marca);
-            }
-            if (placa != null && !placa.trim().isEmpty()) {
-                placa = placa.replace('?', ' ').trim();
-            }
-            if (primeiraPlaca == null || primeiraCor == null || primeiroModelo == null) {
-                primeiraPlaca = placa;
-                primeiraCor = cor;
-                primeiroModelo = modelo;
-            }
-            if (i > 0) {
-                this.salvarOutrosCarros(new CarroVO(placa, cor, modelo));
-            }
-        }
-        return new CarroVO(primeiraPlaca, primeiraCor, primeiroModelo);
-    }
-
-    public void salvarOutrosCarros(CarroVO carroVO) {
-        lista.add(carroVO);
-    }
-
-    /**
-     * Método para retornar aa lista de Carros completa
-     *
-     * @return ArrayList
-     */
-    public ArrayList<CarroVO> retornarListaCarros() {
-        return lista;
-    }
-
-    /**
-     * Remove a linha selecionada na tabela de carros do cliente
-     */
-    public void removeRow() {
-        try {
-            int row = cadastroView.getTable().getSelectedRow();
-            if (row >= 0) {
-                DefaultTableModel model = (DefaultTableModel) cadastroView.getTable().getModel();
-                model.removeRow(row);
-
-                if (ConstHelpers.FLAG == -1) {
-                    CarroVO carro = linhas.get(row);
-
-                    if (daoCarro.excluirPorID(carro.getId())) {
-                        JOptionPane.showMessageDialog(cadastroView, Modificacoes.labelConfig("EXCLUSÂO REALIZADA"),
-                                "Exclusão", JOptionPane.WARNING_MESSAGE);
-                    }
-                }
-            } else {
-                JOptionPane.showMessageDialog(cadastroView,
-                        Modificacoes.labelConfig("Por favor, Selecione uma Linha"),
-                        "Exclusão", JOptionPane.INFORMATION_MESSAGE);
-            }
-        } catch (Exception e) {
-            System.out.println(e.getClass().getSimpleName());
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    public void limparTabelaCarros() {
-        JTable table = cadastroView.getTable();
-        DefaultTableModel model = (DefaultTableModel) table.getModel();
-        int row = table.getRowCount();
-        for (int i = 0; i < row; i++) {
-            model.removeRow(i);
-        }
-    }
-
-    /**
-     * Método para criar um ComboBox dentro da JTable
-     */
-    private void addComboBoxTable_MarcaVO() {
-        JTable table = cadastroView.getTable();
-        TableColumn tableColumn = table.getColumnModel().getColumn(1);
-        tableColumn.setCellEditor(new DefaultCellEditor(new JComboBox<>(preencherCbx_Marca())));
-
-        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
-        renderer.setToolTipText("CLIQUE PARA O COMBO BOX APARECER");
-        tableColumn.setCellRenderer(renderer);
-    }
-
-    /**
-     * Método para criar um ComboBox dentro da Tabela
-     */
-    private void addComboBoxTable_ModeloVO() {
-        JTable table = cadastroView.getTable();
-        TableColumn tableColumn = table.getColumnModel().getColumn(2);
-        JComboBox cbx = new JComboBox(preencherCbx_Modelo());
-        tableColumn.setCellEditor(new DefaultCellEditor(cbx));
-
-        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
-        renderer.setToolTipText("CLIQUE PARA O COMBO BOX APARECER");
-        tableColumn.setCellRenderer(renderer);
-    }
-
-    private void addComboBoxTable_Cores() {
-        JTable table = cadastroView.getTable();
-        TableColumn tableColumn = table.getColumnModel().getColumn(3);
-        JComboBox cbx = new JComboBox(preencherCbx_Cores());
-        cbx.setSelectedIndex(0);
-        tableColumn.setCellEditor(new DefaultCellEditor(cbx));
-
-        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
-        renderer.setToolTipText("CLIQUE PARA O COMBO BOX APARECER");
-        tableColumn.setCellRenderer(renderer);
-    }
-
-    public void addComboBoxJTable() {
-        addComboBoxTable_MarcaVO();
-        addComboBoxTable_ModeloVO();
-        addComboBoxTable_Cores();
-    }
-
-    private DefaultComboBoxModel<String> preencherCbx_Cores() {
-        ArrayList<String> cores = new ArrayList<>();
-        cores.add(ConstInicio.VAZIO);
-        cores.add("AMARELO");
-        cores.add("AZUL");
-        cores.add("BRANCO");
-        cores.add("BEGE");
-        cores.add("CINZA");
-        cores.add("CROMADO");
-        cores.add("LARANJA");
-        cores.add("MARROM");
-        cores.add("PRATA");
-        cores.add("PRETO");
-        cores.add("ROSA");
-        cores.add("ROXO");
-        cores.add("VERMELHO");
-        cores.add("VINHO");
-        return new DefaultComboBoxModel(cores.toArray());
-    }
-
-    /**
-     * Preenche o ComboBox Marca da JTable
-     *
-     * @return new DefaultComboBoxModel
-     */
-    private DefaultComboBoxModel<MarcaVO> preencherCbx_Marca() {
-        BaseDAO<MarcaVO> bDAO = new MarcaDAO();
-        ArrayList<MarcaVO> list = bDAO.consultarTodos();
-        return new DefaultComboBoxModel(list.toArray());
-    }
-
-    /**
-     * Preenche o ComboBox Modelo da JTable
-     *
-     * @return new DefaultComboBoxModel
-     */
-    private DefaultComboBoxModel<ModeloVO> preencherCbx_Modelo() {
-        BaseDAO<ModeloVO> bDAO = new ModeloDAO();
-        ArrayList<ModeloVO> list = bDAO.consultarTodos();
-        return new DefaultComboBoxModel(list.toArray());
-    }
+	public DefaultComboBoxModel<ModeloVO> preencherCbx_Modelo() {
+		BaseDAO<ModeloVO> bDAO = new ModeloDAO();
+		ArrayList<ModeloVO> list = bDAO.consultarTodos();
+		return new DefaultComboBoxModel(list.toArray());
+	}
 
 }
--- src/view/panels/cadastro/subCadastro/PanelzinhoCadastroDados.java
+++ src/view/panels/cadastro/subCadastro/PanelzinhoCadastroDados.java
@@ -1,198 +1,264 @@
 package view.panels.cadastro.subCadastro;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+
 import controller.ControllerCadastroDados;
-import controller.ControllerMainCadastro;
 import model.vo.cliente.ClienteVO;
 import net.miginfocom.swing.MigLayout;
-import util.constantes.Colunas;
 import util.helpers.Modificacoes;
-import util.helpers.Util;
-
-import javax.swing.*;
-import javax.swing.border.LineBorder;
-import javax.swing.table.DefaultTableModel;
-import java.awt.*;
 
 public class PanelzinhoCadastroDados extends JPanel {
 
-    private static final long serialVersionUID = 8795512428702538815L;
-    private ControllerCadastroDados control;
-    private Modificacoes modificacoes;
-    private JTextField txtEmail, txtNome, txtCPF, txtRG, txtTelefone;
-    private JTable table;
-    private JButton btnAddRow, btnRemoveRow;
-    private JCheckBox chckbxBloquear;
+	private static final long serialVersionUID = 8795512428702538815L;
+	private ControllerCadastroDados control;
+	private Modificacoes modificacoes;
+	private JTextField txtEmail, txtNome, txtCPF, txtRG, txtTelefone, txtPlaca;
+	private JCheckBox chckbxBloquear;
+	private JComboBox cbMarca;
+	private JComboBox cbModelo;
+	private JComboBox cbCor;
 
-    public PanelzinhoCadastroDados() {
+	public PanelzinhoCadastroDados() {
 
-        this.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
-        this.setBackground(Color.WHITE);
-        this.setLayout(new MigLayout("", "[grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow]", "[10px][grow][grow][grow][10px][grow][grow][10px][grow][10px][grow][10px][grow][grow][grow][grow][grow][grow]"));
+		this.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		this.setBackground(Color.WHITE);
+		this.setLayout(new MigLayout("", "[grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow][grow]",
+				"[10px][grow][grow][grow][grow][grow][grow][10px][grow][10px][grow][grow][grow][grow][grow][10px]"));
 
-        this.initialize();
-    }
+		this.initialize();
+	}
 
-    private void initialize() {
+	private void initialize() {
 
-        modificacoes = new Modificacoes();
-        control = new ControllerCadastroDados(this);
+		modificacoes = new Modificacoes();
+		control = new ControllerCadastroDados(this);
 
-        setJLabels_JSeparator();
+		setJLabels_JSeparator();
 
-        setInputFields();
+		setInputFields();
 
-        setButtons();
+		setCheckBox();
 
-        setJTable();
+		setComboBox();
 
-        setCheckBox();
+	}
 
-        addListeners();
-    }
+	private void setJLabels_JSeparator() {
+		JLabel lblAdicionarDadosPessoais = new JLabel("Adicionar Dados Pessoais:");
+		lblAdicionarDadosPessoais.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAdicionarDadosPessoais.setFont(new Font("Dialog", Font.BOLD, 26));
+		add(lblAdicionarDadosPessoais, "cell 0 1 5 1,grow");
 
-    private void setJLabels_JSeparator() {
-        JLabel lblNome = new JLabel("<html><body>Nome <a style=color:red>*</a></body></html>");
-        lblNome.setHorizontalAlignment(SwingConstants.CENTER);
-        lblNome.setFont(new Font("Arial", Font.BOLD, 14));
-        this.add(lblNome, "cell 0 1 2 1,grow");
+		JLabel lblNome = new JLabel("<html><body>Nome <a style=color:red>*</a></body></html>");
+		lblNome.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNome.setFont(new Font("Arial", Font.BOLD, 14));
+		this.add(lblNome, "cell 0 2 2 1,grow");
 
-        JLabel lblCpf = new JLabel("<html><body>CPF <a style=color:red>*</a></body></html>");
-        lblCpf.setHorizontalAlignment(SwingConstants.CENTER);
-        lblCpf.setFont(new Font("Arial", Font.BOLD, 14));
-        this.add(lblCpf, "cell 0 2 2 1,grow");
+		JLabel lblCpf = new JLabel("<html><body>CPF <a style=color:red>*</a></body></html>");
+		lblCpf.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCpf.setFont(new Font("Arial", Font.BOLD, 14));
+		this.add(lblCpf, "cell 0 3 2 1,grow");
 
-        JLabel lblRg = new JLabel("RG");
-        lblRg.setHorizontalAlignment(SwingConstants.CENTER);
-        lblRg.setFont(new Font("Arial", Font.BOLD, 14));
-        this.add(lblRg, "cell 0 3 2 1,grow");
+		JLabel lblRg = new JLabel("RG");
+		lblRg.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRg.setFont(new Font("Arial", Font.BOLD, 14));
+		this.add(lblRg, "cell 0 4 2 1,grow");
 
-        JLabel lblEmail = new JLabel("E-Mail");
-        lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
-        lblEmail.setFont(new Font("Arial", Font.BOLD, 14));
-        this.add(lblEmail, "cell 0 5 2 1,grow");
+		JLabel lblEmail = new JLabel("E-Mail");
+		lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEmail.setFont(new Font("Arial", Font.BOLD, 14));
+		this.add(lblEmail, "cell 0 5 2 1,grow");
 
-        JLabel lblTelefone = new JLabel("<html><body>Fone <a style=color:red>*</a></body></html>");
-        lblTelefone.setHorizontalAlignment(SwingConstants.CENTER);
-        lblTelefone.setFont(new Font("Arial", Font.BOLD, 14));
-        this.add(lblTelefone, "cell 0 6 2 1,grow");
+		JLabel lblTelefone = new JLabel("<html><body>Fone <a style=color:red>*</a></body></html>");
+		lblTelefone.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTelefone.setFont(new Font("Arial", Font.BOLD, 14));
+		this.add(lblTelefone, "cell 0 6 2 1,grow");
 
-        JLabel lblAdicionarVeculos = new JLabel("Adicionar Veículos:");
-        lblAdicionarVeculos.setHorizontalAlignment(SwingConstants.CENTER);
-        lblAdicionarVeculos.setFont(new Font("Arial", Font.BOLD, 26));
-        this.add(lblAdicionarVeculos, "cell 0 10 4 1,grow");
-    }
+		JLabel lblAdicionarVeculo = new JLabel("Adicionar Veículo:");
+		lblAdicionarVeculo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAdicionarVeculo.setFont(new Font("Arial", Font.BOLD, 26));
+		this.add(lblAdicionarVeculo, "cell 0 10 4 1,grow");
 
-    private void setInputFields() {
-        txtNome = new JTextField();
-        txtNome.setBorder(new LineBorder(Color.BLACK, 1, true));
-        txtNome.setFont(new Font("Arial", Font.BOLD, 14));
-        this.add(txtNome, "cell 2 1 3 1,grow");
-        txtNome.setColumns(10);
+		JLabel lblModelo = new JLabel("<html><body>Modelo <a style=color:red>*</a></body></html>");
+		lblModelo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblModelo.setFont(new Font("Dialog", Font.BOLD, 14));
+		this.add(lblModelo, "cell 0 14 2 1,grow");
 
-        txtCPF = new JTextField();
-        txtCPF.setBorder(new LineBorder(Color.BLACK, 1, true));
-        txtCPF.setFont(new Font("Arial", Font.BOLD, 14));
-        txtCPF.setHorizontalAlignment(SwingConstants.LEFT);
-        this.add(txtCPF, "cell 2 2 3 1,grow");
-        txtCPF.setColumns(10);
+		JLabel lblPlaca = new JLabel("<html><body>Placa <a style=color:red>*</a></body></html>");
+		lblPlaca.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPlaca.setFont(new Font("Dialog", Font.BOLD, 14));
+		add(lblPlaca, "cell 0 11 2 1,grow");
 
-        txtRG = new JTextField();
-        txtRG.setFont(new Font("Arial", Font.BOLD, 14));
-        txtRG.setBorder(new LineBorder(Color.BLACK, 1, true));
-        this.add(txtRG, "cell 2 3 3 1,grow");
-        txtRG.setColumns(10);
+		JLabel lblCor = new JLabel("<html><body>Cor <a style=color:red>*</a></body></html>");
+		lblCor.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCor.setFont(new Font("Dialog", Font.BOLD, 14));
+		add(lblCor, "cell 0 12 2 1,grow");
 
-        txtEmail = new JTextField();
-        txtEmail.setBorder(new LineBorder(Color.BLACK, 1, true));
-        txtEmail.setFont(new Font("Arial", Font.BOLD, 14));
-        this.add(txtEmail, "cell 2 5 3 1,grow");
-        txtEmail.setColumns(10);
+		JLabel lblMarca = new JLabel("<html><body>Marca <a style=color:red>*</a></body></html>");
+		lblMarca.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMarca.setFont(new Font("Dialog", Font.BOLD, 14));
+		add(lblMarca, "cell 0 13 2 1,grow");
+	}
 
-        txtTelefone = new JTextField();
-        txtTelefone.setFont(new Font("Arial", Font.BOLD, 14));
-        txtTelefone.setBorder(new LineBorder(Color.BLACK, 1, true));
-        this.add(txtTelefone, "cell 2 6 3 1,grow");
-        txtTelefone.setColumns(10);
+	private void setInputFields() {
+		txtNome = new JTextField();
+		txtNome.setBorder(new LineBorder(Color.BLACK, 1, true));
+		txtNome.setFont(new Font("Arial", Font.BOLD, 14));
+		this.add(txtNome, "cell 2 2 4 1,grow");
+		txtNome.setColumns(10);
 
-    }
+		txtCPF = new JTextField();
+		txtCPF.setBorder(new LineBorder(Color.BLACK, 1, true));
+		txtCPF.setFont(new Font("Arial", Font.BOLD, 14));
+		txtCPF.setHorizontalAlignment(SwingConstants.LEFT);
+		this.add(txtCPF, "cell 2 3 4 1,grow");
+		txtCPF.setColumns(10);
 
-    private void setButtons() {
-        btnRemoveRow = new JButton("Remover Carro");
-        btnRemoveRow.setFont(new Font("Dialog", Font.BOLD, 12));
-        add(btnRemoveRow, "cell 7 10 2 1,grow");
+		txtRG = new JTextField();
+		txtRG.setFont(new Font("Arial", Font.BOLD, 14));
+		txtRG.setBorder(new LineBorder(Color.BLACK, 1, true));
+		this.add(txtRG, "cell 2 4 4 1,grow");
+		txtRG.setColumns(10);
 
-        btnAddRow = new JButton("Adicionar Carro");
-        btnAddRow.setFont(new Font("Arial", Font.BOLD, 12));
-        add(btnAddRow, "cell 9 10 2 1,grow");
-    }
+		txtEmail = new JTextField();
+		txtEmail.setBorder(new LineBorder(Color.BLACK, 1, true));
+		txtEmail.setFont(new Font("Arial", Font.BOLD, 14));
+		this.add(txtEmail, "cell 2 5 4 1,grow");
+		txtEmail.setColumns(10);
 
-    private void setJTable() {
-        JScrollPane scrollPane = new JScrollPane();
-        add(scrollPane, "cell 0 12 12 6,grow");
+		txtTelefone = new JTextField();
+		txtTelefone.setFont(new Font("Arial", Font.BOLD, 14));
+		txtTelefone.setBorder(new LineBorder(Color.BLACK, 1, true));
+		this.add(txtTelefone, "cell 2 6 4 1,grow");
+		txtTelefone.setColumns(10);
 
-        scrollPane.setBackground(Color.WHITE);
-        scrollPane.getViewport().setBackground(Color.WHITE);
-        scrollPane.setBorder(new LineBorder(Color.BLACK, 1, true));
+		txtPlaca = new JTextField();
+		txtPlaca.setFont(new Font("Dialog", Font.BOLD, 14));
+		txtPlaca.setColumns(10);
+		txtPlaca.setBorder(new LineBorder(Color.BLACK, 1, true));
+		this.add(txtPlaca, "cell 2 11 4 1,grow");
+	}
 
-        Object[][] data = {{}};
-        table = new JTable(new DefaultTableModel(data, Colunas.COLUNAS_CADASTRO_CLIENTE));
+	private void setCheckBox() {
+		chckbxBloquear = new JCheckBox("Bloquear?");
+		chckbxBloquear.setBackground(Color.WHITE);
+		chckbxBloquear.setHorizontalAlignment(SwingConstants.CENTER);
+		chckbxBloquear.setFont(new Font("Arial", Font.BOLD, 14));
+		this.add(chckbxBloquear, "cell 0 8 2 1,grow");
+	}
 
-        table = modificacoes.tableLookAndFiel(table);
-        control.addComboBoxJTable();
-        Util.maskFormJTable(table.getColumnModel().getColumn(0));
-        scrollPane.setViewportView(table);
-    }
+	private void setComboBox() {
+		cbCor = new JComboBox(control.preencherCbx_Cores());
+		cbCor.setFont(new Font("Dialog", Font.BOLD, 16));
+		cbCor.setBackground(Color.WHITE);
+		add(cbCor, "cell 2 12 4 1,grow");
 
-    private void setCheckBox() {
-        chckbxBloquear = new JCheckBox("Bloquear?");
-        chckbxBloquear.setBackground(Color.WHITE);
-        chckbxBloquear.setHorizontalAlignment(SwingConstants.CENTER);
-        chckbxBloquear.setFont(new Font("Arial", Font.BOLD, 14));
-        this.add(chckbxBloquear, "cell 0 8 2 1,grow");
-    }
+		cbMarca = new JComboBox(control.preencherCbx_Marca());
+		cbMarca.setBackground(Color.WHITE);
+		cbMarca.setFont(new Font("Dialog", Font.BOLD, 16));
+		add(cbMarca, "cell 2 13 4 1,grow");
 
-    private void addListeners() {
-        btnAddRow.addActionListener(e -> control.addrow());
-        btnRemoveRow.addActionListener(e -> control.removeRow());
-    }
+		cbModelo = new JComboBox(control.preencherCbx_Modelo());
+		cbModelo.setFont(new Font("Dialog", Font.BOLD, 16));
+		cbModelo.setBackground(Color.WHITE);
+		add(cbModelo, "cell 2 14 4 1,grow");
+	}
 
-    public ClienteVO returnForm() {
-        return control.getFormCliente();
-    }
+//	public ClienteVO returnForm() {
+//	return control.getFormCliente();
+//}
 
-    public Modificacoes getModificacoes() {
-        return modificacoes;
-    }
+//public Modificacoes getModificacoes() {
+//	return modificacoes;
+//}
 
-    public JTextField getTxtEmail() {
-        return txtEmail;
-    }
+	public JTextField getTxtEmail() {
+		return txtEmail;
+	}
 
-    public JTextField getTxtNome() {
-        return txtNome;
-    }
+	public void setTxtEmail(JTextField txtEmail) {
+		this.txtEmail = txtEmail;
+	}
 
-    public JTextField getTxtCPF() {
-        return txtCPF;
-    }
+	public JTextField getTxtNome() {
+		return txtNome;
+	}
 
-    public JTextField getTxtRG() {
-        return txtRG;
-    }
+	public void setTxtNome(JTextField txtNome) {
+		this.txtNome = txtNome;
+	}
 
-    public JTextField getTxtTelefone() {
-        return txtTelefone;
-    }
+	public JTextField getTxtCPF() {
+		return txtCPF;
+	}
 
-    public JCheckBox getChckbxBloquear() {
-        return chckbxBloquear;
-    }
+	public void setTxtCPF(JTextField txtCPF) {
+		this.txtCPF = txtCPF;
+	}
 
-    public JTable getTable() {
-        return table;
-    }
+	public JTextField getTxtRG() {
+		return txtRG;
+	}
 
-    public void setTable(JTable table) {
-        this.table = table;
-    }
+	public void setTxtRG(JTextField txtRG) {
+		this.txtRG = txtRG;
+	}
+
+	public JTextField getTxtTelefone() {
+		return txtTelefone;
+	}
+
+	public void setTxtTelefone(JTextField txtTelefone) {
+		this.txtTelefone = txtTelefone;
+	}
+
+	public JTextField getTxtPlaca() {
+		return txtPlaca;
+	}
+
+	public void setTxtPlaca(JTextField txtPlaca) {
+		this.txtPlaca = txtPlaca;
+	}
+
+	public JCheckBox getChckbxBloquear() {
+		return chckbxBloquear;
+	}
+
+	public void setChckbxBloquear(JCheckBox chckbxBloquear) {
+		this.chckbxBloquear = chckbxBloquear;
+	}
+
+	public JComboBox getCbMarca() {
+		return cbMarca;
+	}
+
+	public void setCbMarca(JComboBox cbMarca) {
+		this.cbMarca = cbMarca;
+	}
+
+	public JComboBox getCbModelo() {
+		return cbModelo;
+	}
+
+	public void setCbModelo(JComboBox cbModelo) {
+		this.cbModelo = cbModelo;
+	}
+
+	public JComboBox getCbCor() {
+		return cbCor;
+	}
+
+	public void setCbCor(JComboBox cbCor) {
+		this.cbCor = cbCor;
+	}
+
 }
