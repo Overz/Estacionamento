6d787839e9050593f8f5981fb22de45c46b5b7c3
Christian Lima <christian@projetabrasil.net.br>
2020-07-10 11:37:38 -0300

    desfazendo a caca

--- py_script/ocr.py
+++ py_script/ocr.py
@@ -14,7 +14,7 @@ teste2 = '/home/cris/tess/blueplate/bp2.png'
 def main(path):
     if path != "":
         try:
-            matches(path)
+            matches(path)  # passar o teste no path para testar
         except Exception as ex2:
             print("Py: Error trying process the Image")
             print(ex2)
@@ -29,19 +29,24 @@ def replaces(string):
 
 
 # Leitura da Imagem
-def matches(path):  # Utilizar o 'path' no lugar do 'teste'
-    regex = '([A-Z0-9]{7})|([A-Z-0-9]{8})'  # Regex para placas
-    image_text = pytesseract.image_to_string(Image.open(str(teste2)))  # Realiza a Leitura
+def matches(path):
+    regex = '([A-Z0-9]{7})'  # Mercosul
+    regex2 = '([A-Z-0-9]{8})'  # Normal
+    image_text = pytesseract.image_to_string(Image.open(str(path)))  # Realiza a Leitura
     matched = str(replaces(re.findall(regex, image_text)))  # Match com o regex e replace
+    matched2 = str(replaces(re.findall(regex2, image_text)))  # Match com o regex e replace
     if matched:
         print(image_text)
         print(matched)
+    if matched2:
+        print(image_text)
+        print(matched2)
 
 
-# Main Method
+# Main Exe
 if __name__ == '__main__':
     try:
-        main(path='sys.argv[1]')  # Remover as aspas ao utilizar no programa
+        main(path=sys.argv[1])  # Remover as aspas ao utilizar no programa: 'sys.argv[1]'
     except Exception as ex1:
         print("Py: How to run: python <script> <args> \n<args> need to be a image .png, .jpeg, .jpg")
         print(ex1)
